e5ba4868f0866f082bcc8c5d393298ba580d3b24
Primal System Studio <primalsystemstudio@protonmail.com>
2021-02-03 19:04:48 +0000

    Added handlers
    Handlers for indexing rooms, views and names

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,53 @@
-extern crate crabler;
-
 use crabler::*;
-use async_std::future;
+
+const ENTRY_PREFIX: &'static str = "https://chaturbate.com/tags/";
 
 #[derive(WebScraper)]
 #[on_response(response_handler)]
-#[on_html(".tag_row", entry_handler)]
-
-const ENTRY_PREFIX: &'static str = "https://chaturbate.com/tags/";
+#[on_html(".tag_row .tag", name_handler)]
+#[on_html(".tag_row .viewer", viewer_handler)]
+#[on_html(".tag_row .rooms", room_handler)]
 
 struct Scraper {}
 
 impl Scraper {
-    fn on_response(&self, response: Response) -> Result<()> {
-        if let Some(destination) = response.download_destination {
-            if response.status == 200 {
-                warn!("Finished downloading {} -> {}", response.url, destination);
-            }
+    // add more comments
+    async fn response_handler(&self, response: Response) -> Result<()> {
+        println!("Status {}", response.status);
+        Ok(())
+    }
+
+// WORK HERE
+    async fn name_handler(&self, response: Response, tag_name: Element) -> Result<()> {
+        if let Some(s) = tag_name.tag("a") {
+            //get names
+            println!("{}", )
+
         }
 
         Ok(())
     }
+    async fn viewer_handler(&self, response: Response, tag_views: Element) -> Result<()> {
+        if let Some(span) = tag_row.tag("span") {
+            //get views
+        }
 
-    async fn entry_handler(&self, mut response: Response, row: Element) -> Result<()> {
-        // What to do with each tag_row entry ?
-        // Store in .csv
+        Ok(())
+    }
+    async fn room_handler(&self, response: Response, tag_room: Element) -> Result<()> {
+        if let Some(rooms) = tag_row.text {
+            
+        }
 
         Ok(())
     }
 }
 
 
+
 #[async_std::main]
 async fn main() -> Result<()> {
-    // Add "?page = {#}" until # = 10
-
-
+    // Add "?page=#" until # = 10
+    let scraper = Scraper {};
+    scraper.run(Opts::new().with_urls(vec![ENTRY_PREFIX])).await
 }
