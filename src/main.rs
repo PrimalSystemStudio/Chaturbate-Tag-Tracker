f1764b4e778b9898c514d2390e4f8461ac694bcf
Primal System Studio <primalsystemstudio@protonmail.com>
2021-02-06 17:46:48 +0000

    Created single tag handler

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,26 @@ use crabler::*;
 
 const ENTRY_PREFIX: &'static str = "https://chaturbate.com/tags/";
 
+// Use WebScraper trait to get each item with the ".tag_row" class
 #[derive(WebScraper)]
 #[on_response(response_handler)]
-#[on_html(".tag_row .tag", name_handler)]
-#[on_html(".tag_row .viewer", viewer_handler)]
-#[on_html(".tag_row .rooms", room_handler)]
+#[on_html(".tag_row", tag_handler)]
 
 struct Scraper {}
 
 impl Scraper {
-    // add more comments
+    // Print webpage status
     async fn response_handler(&self, response: Response) -> Result<()> {
         println!("Status {}", response.status);
         Ok(())
     }
 
-// WORK HERE
-    async fn name_handler(&self, response: Response, tag_name: Element) -> Result<()> {
-        if let Some(s) = tag_name.tag("a") {
-            //get names
-            println!("{}", )
-
+    async fn tag_handler(&self, response: Response, el: Element) -> Result<()> {
+        // Print all tag info
+        if let Some(tag_data) = el.children() {
+            let tag_name = tag_data.first().text();
+            print("{}", tag_name)
         }
-
-        Ok(())
-    }
-    async fn viewer_handler(&self, response: Response, tag_views: Element) -> Result<()> {
-        if let Some(span) = tag_row.tag("span") {
-            //get views
-        }
-
-        Ok(())
-    }
-    async fn room_handler(&self, response: Response, tag_room: Element) -> Result<()> {
-        if let Some(rooms) = tag_row.text {
-            
-        }
-
         Ok(())
     }
 }
