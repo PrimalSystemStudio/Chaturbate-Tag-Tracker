d57916c4d43125a2a7d54e42f27a27f87d848cdf
Primal System Studio <primalsystemstudio@protonmail.com>
2021-02-22 17:38:25 +0000

    Added py function for cleaning and loading data
    Added clean() function for loading data to internal memory and cleaning it.

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,9 +1,10 @@
 import sqlite3
+import pandas as pd
 
 # For adding new data to the table
 def add(tag_name, tag_views, tag_rooms, date_time):
     # Open database
-    connec = sqlite3.connect('cb_log.db')
+    connec = sqlite3.connect('src/cb_log.db')
     cursor = connec.cursor()
 
     # Add tag data to database [exceptions??]
@@ -17,14 +18,23 @@ def add(tag_name, tag_views, tag_rooms, date_time):
     connec.close()
     return 1
 
-# For cleaning data before displaying it
-## Make function
-### make internal memory of data
+# For loading and cleaning data before displaying it
+def clean():
+    # Open database
+    connec = sqlite3.connect('src/cb_log.db')
+    cursor = connec.cursor()
+
+    # Make dataframe of all tags with viewers greater than 10
+    query = "SELECT * FROM cb_log WHERE viewers > 10"
+    df = pd.read_sql_query(query, connec)
+
+    # Close database
+    connec.close()
+
 ### views/room ratio
-### remove low traffic tags
 ### Add notes for high traffic tags/too many rooms etc.
 
 # For sending cleaned data to Rust
 
 if __name__ == "__main__":
-        add("#sexy", 100, 200, "12:00 16-02-2020")
+    clean()
