53b48e3498ef42a468f60c002b6b9d39cdef9e9c
Primal System Studio <primalsystemstudio@protonmail.com>
2021-03-26 12:57:25 +0000

    File modification
    Moved scraping code from main.rs to scrape.rs

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,5 +1,8 @@
 import sqlite3
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # For adding new data to the table
 def add(tag_name, tag_views, tag_rooms, date, time):
@@ -31,9 +34,10 @@ def clean():
     # Make column for viewers/rooms ratio
     ratio = df['viewers'] / df['rooms']
     df.insert(loc=len(df.columns), column='ratio', value = ratio)
-    print(df.head())
 
-    
+    # Sort by highest ratio
+    print(df.sort_values(by='ratio', ascending=False).head())
+
     # Close database
     connec.close()
 
